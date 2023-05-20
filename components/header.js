b473bc7a0fc213ffbeb367d8d28c82ef23f5ac16
Adam Ansari <adamansari@Adams-MacBook-Pro.local>
2023-05-20 22:58:50 +0530

    Header component

--- components/header.js
+++ components/header.js
@@ -1,7 +1,47 @@
-import React from "react";
+import Image from "next/image";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="shadow-md">
+      <div
+        className="w-[1200px] mx-[auto]
+      flex flex-row justify-between items-center"
+      >
+        <div className="py-[20px] cursor-pointer">
+          <Image
+            src={"/images/jm-logo-light.png"}
+            alt={"JMF Logo"}
+            width={157}
+            height={35}
+          />
+        </div>
+        <div className="flex">
+          <div className="flex items-center pl-[10px] pr-[10px] cursor-pointer">
+            <div className="pr-[5px] py-[20px]">Products</div>
+            <div>
+              <Image
+                src={"/images/down-arrow.png"}
+                alt={"JMF Logo"}
+                width={10}
+                height={10}
+              />
+            </div>
+          </div>
+          <div className="pl-[10px] pr-[10px] py-[20px] cursor-pointer">
+            About Us
+          </div>
+        </div>
+        <div
+          className="border border-[#0E75BD] rounded-md
+        px-[20px] py-[8px] cursor-pointer
+        transition ease-in	
+        hover:bg-[#0E75BD] hover:text-white"
+        >
+          Get Started
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
